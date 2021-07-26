bf192503e9223818fc7f12c135b84b9dc4b4ba80
승핵 <sping__@naver.com>
2021-07-26 09:45:22 +0900

    원 그리기
    원 그리기

---
+++ LetsGoFriendsProject/Assets/Scripts/DrawCircle.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCircle : MonoBehaviour
+{
+    public int segments;
+    public float xradius;
+    public float yradius;
+    LineRenderer line;
+
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+        line.positionCount = (segments + 1);
+        line.useWorldSpace = false;
+        CreatePoints();
+    }
+
+    void CreatePoints()
+    {
+        float x;
+        float y;
+        float z = 0f;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Cos(Mathf.Deg2Rad * angle) * xradius;
+            y = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+
+            line.SetPosition(i, new Vector3(x, y, z));
+            angle += (360f / segments);
+        }
+    }
+}
+

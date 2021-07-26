a78bc317359af21ef7ce760c20b886643d5cfecc
승핵 <sping__@naver.com>
2021-07-26 09:45:56 +0900

    방향전환 기능추가
    방향전환 기능추가

---
+++ LetsGoFriendsProject/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("속도, 반지름")]
+
+    [SerializeField] [Range(0f, 10f)] private float speed = 1;
+    [SerializeField] [Range(0f, 10f)] private float radius = 1;
+
+    private float runningTime = 0;
+    private Vector2 newPos = new Vector2();
+
+    private void Update()
+    {
+        runningTime += Time.deltaTime * speed;
+        float x = radius * Mathf.Cos(runningTime);
+        float y = radius * Mathf.Sin(runningTime);
+        newPos = new Vector2(x, y);
+        this.transform.position = newPos;
+    }
+
+    public void ChangeDirection()
+    {
+        speed = speed * -1f;
+    }
+}
